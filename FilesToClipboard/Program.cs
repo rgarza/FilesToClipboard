dbeda027bed176b256b85a91e253002e35a04af5
unknown <rd60920@WDXCNU2012FBW0.jdnet.deere.com>
2012-05-24 14:38:41 -0500

    Initial Commit

---
+++ FilesToClipboard/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+
+namespace FilesToClipboard
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(String [] args)
+        {
+            if (args.Length > 0)
+            {
+                String file = args[0];
+                if (File.Exists(file) || Directory.Exists(file))
+                {
+                    StringCollection paths = new StringCollection();
+                    paths.Add(file);
+                    Clipboard.SetFileDropList(paths);
+                }
+            }
+        }
+    }
+}
